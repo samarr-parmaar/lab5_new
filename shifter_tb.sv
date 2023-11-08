c6acc071a7d3f4dd50e996d6ea285076d712f5a3
littleguac <123615242+littleguac@users.noreply.github.com>
2023-11-07 17:13:30 -0800

    Update shifter_tb.sv

--- shifter_tb.sv
+++ shifter_tb.sv
@@ -4,11 +4,11 @@ module shifter_tb;
 	reg [1:0] shift;
 	reg err;
 	wire [15:0] sout;
-
+	//initializes shifter module
 	shifter DUT(.in (in),.shift (shift),.sout (sout));
 
 	task check;
-
+	//checks for output from shifter module
 	input [15:0] expected_sout;
 	
 	begin
@@ -27,26 +27,26 @@ module shifter_tb;
 	initial begin
 	
 	err = 1'b0;
-
+	
 	in = 16'b1111000011001111;
 	shift = 2'b00;
 	#10;
-
+	//checks to see that if shift inout is 00, that no operation is done
 	check( 16'b1111000011001111 );
 
 	shift = 2'b01;
 	#10;
-
+	//if shift = 01 that it is shifted to the right
 	check( 16'b1110000110011110 );
 
 	shift = 2'b10;
 	#10;
-
+	//if shift = 10 that it is shifted to the left with MSB as 0
 	check( 16'b0111100001100111 );
 
 	shift = 2'b11;
 	#10;
-
+	//if shift = 10 that it is shifted to the left
 	check( 16'b1111100001100111 );
 
 
